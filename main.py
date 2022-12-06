a4ad5300511f1f9cce5dda129d214907ef3eb66e
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2022-12-06 22:47:14 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from googletrans import Translator
+import googletrans
+
+
+def print_output(sentence):
+    print(googletrans.LANGUAGES)
+    translator = Translator()
+    supported = ["hi","or","bn","kn","mr","ne", "pa","gu","ta","te", "as"]
+    translated =[]
+    for language in supported:
+        translate= translator.translate(sentence, dest=language)
+        print(translate.text)
+        translated.append(translate)
+    return translated
+
+if __name__ == '__main__':
+    print(print_output('Hello, world'))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
